f50925cea557fe4950af23ec72743b1124822e5e
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-09 21:19:13 -0700

    Add project files.

---
+++ ConsoleAppAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating welcome request and converting into boolean answer to match
+            Console.WriteLine("Guess my age?");
+            int myAge = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = myAge == 42;
+            //the do while 
+            do
+            {   //switch statements to have for the contents of the program
+                switch (myAge)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62, try again.");
+                        Console.WriteLine("Guess a number?");
+                        myAge = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29, try again.");
+                        Console.WriteLine("Guess a number?");
+                        myAge = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55, try again.");
+                        Console.WriteLine("Guess a number?");
+                        myAge = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 42:
+                        Console.WriteLine("You guessed 42, that is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        myAge = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+
+
+            //to keep the program running though the switches
+            while (!isGuessed);
+            Console.Read();
+        }
+    }
+}
